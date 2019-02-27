017eaafe1da48867839d004c3d608fc2184e004a
LevensLovato <levens.lovato@comcast.net>
2019-02-27 17:57:50 -0600

    Python Script for trimming strings.

---
+++ Strings Trimming.py
@@ -0,0 +1,32 @@
+s = '->Levens<-'
+
+print(' ')
+print('Original.: ->Levens<-') 
+print(' ')
+
+a = s[:-2]			#Remove The Last Two Characters Of A String...
+print('A[:-2]...: ' + a)
+
+b = s[2:]			#Remove The First Two Characters Of A String...
+print('B[2:]....: ' + b)
+
+c = s[2]			#Gets The Second Character Of A String...
+print('C[2].....: ' + c)
+
+d = s[2:-2]			#Remove The First Two & Last Two Characters Of A String...
+print('D[2:-2]..: ' + d)
+
+f = s[0:5]			#Get From 1st Position To The 5th Position...
+print('F[0:5]...: ' + f)
+
+g = s[0:1]			#Get First Character From String...
+print('G[0:1]...: ' + g)
+
+h = s[-4:]			#Get The Last 4 Characters Of String...
+print('H[-4:]...: ' + h)
+
+i = s[-1:]			#Get Last Character Of String...
+print('I[-1:]...: ' + i)
+
+print(' ')
+print('Process Completed...')
